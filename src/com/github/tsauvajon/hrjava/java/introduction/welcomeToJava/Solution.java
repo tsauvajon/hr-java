d96cf3faac46ea12d7947095afdfbfee13e830dc
Thomas Sauvajon <thomas.sauvajon@gmail.com>
2017-08-22 16:32:43 +0200

    Created project structure + added 4 challenges

---
+++ src/com/github/tsauvajon/hrjava/java/introduction/welcomeToJava/Solution.java
@@ -0,0 +1,9 @@
+package com.github.tsauvajon.hrjava.java.introduction.welcomeToJava;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}

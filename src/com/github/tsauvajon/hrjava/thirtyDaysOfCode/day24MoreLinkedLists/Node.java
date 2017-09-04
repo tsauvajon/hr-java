b238edddbc3bf8f31ac0f761b2f03affbcbd489b
Thomas Sauvajon <thomas.sauvajon@gmail.com>
2017-09-04 16:41:13 +0200

    Added day 24

---
+++ src/com/github/tsauvajon/hrjava/thirtyDaysOfCode/day24MoreLinkedLists/Node.java
@@ -0,0 +1,12 @@
+package com.github.tsauvajon.hrjava.thirtyDaysOfCode.day24MoreLinkedLists;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int d) {
+        data = d;
+        next = null;
+    }
+
+}
---
+++ src/com/github/tsauvajon/hrjava/thirtyDaysOfCode/day24MoreLinkedLists/Solution.java
@@ -0,0 +1,56 @@
+package com.github.tsauvajon.hrjava.thirtyDaysOfCode.day24MoreLinkedLists;
+
+import java.util.Scanner;
+
+class Solution {
+    public static Node removeDuplicates(Node head) {
+        Node root = head;
+
+        while (head != null) {
+            while (head.next != null && head.next.data == head.data) {
+                head.next = head.next.next;
+            }
+
+            head = head.next;
+        }
+
+        return root;
+    }
+
+    public static Node insert(Node head, int data) {
+        Node p = new Node(data);
+        if (head == null)
+            head = p;
+        else if (head.next == null)
+            head.next = p;
+        else {
+            Node start = head;
+            while (start.next != null)
+                start = start.next;
+            start.next = p;
+
+        }
+        return head;
+    }
+
+    public static void display(Node head) {
+        Node start = head;
+        while (start != null) {
+            System.out.print(start.data + " ");
+            start = start.next;
+        }
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        Node head = null;
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            int ele = sc.nextInt();
+            head = insert(head, ele);
+        }
+        head = removeDuplicates(head);
+        display(head);
+
+    }
+}
